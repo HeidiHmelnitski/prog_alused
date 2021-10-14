51039ab6357f74e511152e7142c494dfecf53f06
Heidi <heidi.hmelnitski@khk.ee>
2021-10-14 09:41:58 +0300

    teksti_väljastamine

--- app.js
+++ app.js
@@ -12,3 +12,18 @@ console.log("see on teine rida")
 
 
 console.log("see "+" on "+" liitlause")
+
+console.log("Aastal 2016 valiti presidendiks Kersti Kaljulaid")
+// 1. deklareerimine
+//let muutujanimi
+//väärtuse omistamine
+//muutujanimi = väärtus
+
+
+let aasta
+aasta = 2016
+
+let president
+president = "Kersti Kaljulaid"
+
+console.log(aasta + ' Eesti presidendiks valiti ' + president)
